083b7dd317180b31d7ca08d1c48514ed577b3c77
Marin Koppert <Marinkoppert@hotmail.com>
2025-05-11 20:42:16 +0200

    added entry point mqtt handler

--- app/src/main/java/com/example/myapplication/receivers/AlarmReceiver.kt
+++ app/src/main/java/com/example/myapplication/receivers/AlarmReceiver.kt
@@ -3,16 +3,18 @@ package com.example.myapplication.receivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.example.myapplication.data.network.mqtt.MqttHandler
-import javax.inject.Inject
+import com.example.myapplication.di.MqttHandlerEntryPoint
+import dagger.hilt.android.EntryPointAccessors
 
 class AlarmReceiver : BroadcastReceiver() {
-    @Inject
-    lateinit var mqttHandler: MqttHandler
     override fun onReceive(context: Context, intent: Intent) {
+        val entryPoint = EntryPointAccessors.fromApplication(
+            context.applicationContext,
+            MqttHandlerEntryPoint::class.java
+        )
+
+        val mqttHandler = entryPoint.mqttHandler()
         val topic = intent.getStringExtra("topic") ?: return
-
-
         mqttHandler.publishMessage("1", topic)
     }
 
